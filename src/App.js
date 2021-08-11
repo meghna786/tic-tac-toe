7c62f3bd7eb65b260b3fdc5ada732628a13d6535
Meghna <meghna.sharma019@gmail.com>
2021-08-11 12:29:43 +0530

    Added board and square component, configured eslint and prettier

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Board from './components/Board';
+
+import './styles/root.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>message</h2>
+      <Board />
+    </div>
+  );
+};
+
+export default App;
