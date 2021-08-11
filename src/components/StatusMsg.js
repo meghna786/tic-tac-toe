3381c8fa50b785edd1f777e82339c9324537e2e6
Meghna <meghna.sharma019@gmail.com>
2021-08-11 18:07:42 +0530

    added styles

--- src/components/StatusMsg.js
+++ src/components/StatusMsg.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-const StatusMsg = ({winner, current}) => {
-      // const msg = winner
-      // ? `The winner is ${winner}`
-      // : `The next player is ${current.isXNext ? 'X' : 'O'}`;
-      const NoMovesLeft=current.board.every(el=> el!==null);
-      return (
-            <h2>{winner && `The winner is ${winner}`}
-            {!winner && NoMovesLeft && `X and O tied`}
-            {!winner && !NoMovesLeft && `The next player is ${current.isXNext ? 'X' : 'O'}`}</h2>
-      )
-}
+const StatusMsg = ({ winner, current }) => {
+  // const msg = winner
+  // ? `The winner is ${winner}`
+  // : `The next player is ${current.isXNext ? 'X' : 'O'}`;
+  const NoMovesLeft = current.board.every(el => el !== null);
+  return (
+    <div className="status-message">
+      {winner && (
+        <>
+          The Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && NoMovesLeft && (
+        <>
+          <span className="text-green">X</span> and{' '}
+          <span className="text-orange">O</span> tied
+        </>
+      )}
+      {!winner && !NoMovesLeft && (
+        <>
+          The next player is{' '}
+          <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+            {current.isXNext ? 'X' : 'O'}
+          </span>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default StatusMsg
+export default StatusMsg;
